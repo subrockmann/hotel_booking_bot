6de98f782793d4c60490058157ffe4ae184ff01e
subrockmann <newinmunich@aol.com>
2022-06-24 21:22:53 +0200

    fix: remove intent num_nights

--- actions/actions.py
+++ actions/actions.py
@@ -4,18 +4,22 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-from typing import Text, Any, Dict
+from typing import Any, Text, Dict, List
 
-from rasa_sdk import Tracker, ValidationAction, FormValidationAction
+from rasa_sdk import Action, Tracker, ValidationAction, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
 
 from datetime import datetime as dt
 
 def get_date(time):
-    time_object = dt.strptime(time, "%Y-%m-%dT%H:%M:%S.%f%z")
-    date = str(time_object.date())
-    return date
+    try:
+        time_object = dt.strptime(time, "%Y-%m-%dT%H:%M:%S.%f%z")
+        date = str(time_object.date())
+        return date
+    except:
+        return "get_date_exception"
 
 
 class ValidateStayForm(FormValidationAction):
@@ -57,23 +61,54 @@ class ValidateStayForm(FormValidationAction):
             # validation failed, set this slot to None
             return {"checkout_date": get_date(slot_value)}
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ValidatePredefinedSlots(ValidationAction):
+    def validate_checkin_date(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate checkin_date."""
+        if isinstance(slot_value, dict):
+            # DucklingEntityExtractor returns a dict when it extracts a date range
+            return [{"checkin_date": get_date(slot_value["from"])}, 
+                {"checkout_date": get_date(slot_value["to"])}]
+            #return {"checkin_date": slot_value.capitalize()}
+        else:
+            # validation failed, set this slot to None
+            return {"checkin_date": get_date(slot_value)}
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
+
+# class EmailValidation(ValidationAction):
+# #
 #     def name(self) -> Text:
-#         return "action_hello_world"
-#
+#         return "validate_email"
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+
+#         # validate the email address
+#         entities = tracker.latest_message['entities']
+#         dispatcher.utter_message(text=entities)
+
 #         return []
+
+
+class ActionCheckRoom(Action):
+    def name(self) -> Text:
+        return "action_check_rooms"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        num_guests = tracker.get_slot('num_guests')
+        #q = "select * from restaurants where cuisine='{0}' limit 1".format(cuisine)
+        #result = db.query(q)
+        message = f"Number of guests: {num_guests}"
+        dispatcher.utter_message(text=message)
+        #return [SlotSet("matches", result if result is not None else [])]
+        return
