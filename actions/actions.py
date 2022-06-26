544af07e256cc6f7046a2f9e4c0bd072b74aa7c3
subrockmann <newinmunich@aol.com>
2022-06-26 18:46:44 +0200

    feat: added email validation

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,8 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from html import entities
+import re
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, ValidationAction, FormValidationAction
@@ -12,14 +14,16 @@ from rasa_sdk.types import DomainDict
 from rasa_sdk.events import SlotSet
 
 from datetime import datetime as dt
+from email_validator import validate_email, EmailNotValidError
 
 def get_date(time):
+    # check if the date needs to be cleaned up
     try:
         time_object = dt.strptime(time, "%Y-%m-%dT%H:%M:%S.%f%z")
         date = str(time_object.date())
         return date
     except:
-        return "get_date_exception"
+        return time
 
 
 class ValidateStayForm(FormValidationAction):
@@ -35,12 +39,14 @@ class ValidateStayForm(FormValidationAction):
         ) -> Dict[Text, Any]:
         """Validate checkin_date."""
         if isinstance(slot_value, dict):
+            print("valdiate Stay form - checkin date - dict")
             # DucklingEntityExtractor returns a dict when it extracts a date range
             return [{"checkin_date": get_date(slot_value.get("from"))}, 
                 {"checkout_date": get_date(slot_value.get("to"))}]
             #return {"checkin_date": slot_value.capitalize()}
         else:
             # validation failed, set this slot to None
+            print("valdiate Stay form - checkin date -  no dict")
             return {"checkin_date": get_date(slot_value)}
 
     def validate_checkout_date(
@@ -61,6 +67,40 @@ class ValidateStayForm(FormValidationAction):
             # validation failed, set this slot to None
             return {"checkout_date": get_date(slot_value)}
 
+class ValidateEmailForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_email_form"
+
+    def validate_email(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+        ) -> Dict[Text, Any]:
+        """Validate email."""
+        try: 
+            # Validate & take the normalized form of the email
+            # address for all logic beyond this point (especially
+            # before going to a database query where equality
+            # does not take into account normalization).
+            entities = tracker.latest_message['entities']
+            print(entities)
+            #email = validate_email(entities.get("email")).email
+            #email = validate_email(slot_value).email
+            #dispatcher.utter_template("utter_email", tracker)
+            dispatcher.utter_message(response="utter_email")
+            return {"email": entities} #email}
+        except EmailNotValidError as e:
+            # email is not valid, exception message is human-readable
+            print(str(e))
+            dispatcher.utter_message(text= "Validate Email Form") ### TODO
+            dispatcher.utter_message(response="utter_no_email")
+            #dispatcher.utter_template("utter_no_email", tracker)
+            dispatcher.utter_message(text=str(e))
+            return {"email": None}
+
+
 class ValidatePredefinedSlots(ValidationAction):
     def validate_checkin_date_dummy(
         self,
@@ -72,7 +112,7 @@ class ValidatePredefinedSlots(ValidationAction):
         """Validate checkin_date_dummy."""
         # DucklingEntityExtractor returns a dict when it extracts a date range
         if isinstance(slot_value, dict):
-            print("Is dict:")
+            print(f"Is dict: {slot_value}")
             #tracker.set_slot("checkout_date", get_date(slot_value.get("to")))  ### FIX: 'Tracker' object has no attribute 'set_slot'
             #SlotSet("checkout_date", get_date(slot_value.get("to")))  ### FIX: This crashes 
             
@@ -89,6 +129,51 @@ class ValidatePredefinedSlots(ValidationAction):
             return {"checkin_date": get_date(slot_value)}
             #return SlotSet("checkin_date", get_date(slot_value))
 
+    def validate_email(
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+            ) -> Dict[Text, Any]:
+            """Validate email."""
+            try: 
+                # Validate & take the normalized form of the email
+                # address for all logic beyond this point (especially
+                # before going to a database query where equality
+                # does not take into account normalization).
+                entities = tracker.latest_message['entities']
+                print(entities)
+                #email = validate_email(entities.get("email")).email
+                #email = validate_email(slot_value).email
+                #print(email)
+                dispatcher.utter_template("utter_email", tracker)
+            except EmailNotValidError as e:
+                # email is not valid, exception message is human-readable
+                print(str(e))
+                dispatcher.utter_template("utter_no_email", tracker)
+                dispatcher.utter_message(text=str(e))
+                dispatcher.utter_message(text= "Validate predefined slots")
+                return {"email": None}
+
+
+
+    # def validate_email(  ### is replaced by form validation for email form
+    #         self,
+    #         slot_value: Any,
+    #         dispatcher: CollectingDispatcher,
+    #         tracker: Tracker,
+    #         domain: DomainDict,
+    #     ) -> Dict[Text, Any]:
+    #         """Validate email address."""
+    #         if isinstance(slot_value, dict):
+    #             print(f"Is dict: {slot_value}")                
+    #             return {"checkin_date": get_date(slot_value.get("from"))}
+    #         else:
+    #             print("is no dict")
+    #             return {"checkin_date": get_date(slot_value)}
+
+
 
 # class EmailValidation(ValidationAction):
 # #
@@ -104,7 +189,6 @@ class ValidatePredefinedSlots(ValidationAction):
 #         dispatcher.utter_message(text=entities)
 
 #         return []
-
 
 class ActionCheckRoom(Action):
     def name(self) -> Text:
